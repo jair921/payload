085acae1e928805d91b99fb139cf1c99167f83dc
Jair Andres Martinez Manzano <jair.martinez@evertecinc.com>
2021-05-10 11:53:09 -0500

    Add exceptions for hex and eval functions

--- app/Domain/Payload/Actions/PayloadToArrayAction.php
+++ app/Domain/Payload/Actions/PayloadToArrayAction.php
@@ -2,14 +2,23 @@
 
 namespace App\Domain\Payload\Actions;
 
+use App\Domain\Payload\Exceptions\StorePayloadException;
+use ErrorException;
+use ParseError;
+
 class PayloadToArrayAction
 {
     public function __invoke(string $data)
     {
         $result = [];
-        $str = hex2bin(str_replace(["\n", "\r"], '', $data));
-
-        eval(("\$result = [".$str."];"));
+        try{
+            $str = hex2bin(str_replace(["\n", "\r"], '', $data));
+            eval(("\$result = [".$str."];"));
+        }catch (ErrorException $errorException){
+            throw StorePayloadException::hexFailDecode($errorException->getMessage());
+        } catch (ParseError $parseError){
+            throw StorePayloadException::evalFailDecode($parseError->getMessage());
+        }
 
         return $result;
     }
--- app/Http/Controllers/PayloadController.php
+++ app/Http/Controllers/PayloadController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Domain\Payload\Actions\StorePayloadAction;
+use App\Domain\Payload\Exceptions\StorePayloadException;
 use App\Location;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as ResponseCodes;
+use Illuminate\Support\Facades\Response;
 use Spatie\QueryBuilder\AllowedFilter;
 use Spatie\QueryBuilder\QueryBuilder;
 
 class PayloadController extends Controller
 {
-
     public function index(Request $request)
     {
         $locations = QueryBuilder::for(Location::class)
@@ -26,7 +28,13 @@ class PayloadController extends Controller
     public function store(Request $request, StorePayloadAction $storePayloadAction)
     {
        $data = file_get_contents('php://input');
+        try {
+            $created = ($storePayloadAction)($data);
+        }catch (StorePayloadException $storePayloadException){
+            return Response::json(['message' => $storePayloadException->getMessage()], ResponseCodes::HTTP_UNPROCESSABLE_ENTITY);
+        }
 
-        return ($storePayloadAction)($data);
+        return $created ? Response::json(['message' => trans('payload.store_success')], ResponseCodes::HTTP_CREATED)
+        : Response::json(['message' => trans('payload.store_fail')], ResponseCodes::HTTP_UNPROCESSABLE_ENTITY);
     }
 }

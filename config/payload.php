ad8fa30c829dc88f11af41f848e816bbb4f1a84d
Jair Andres Martinez Manzano <jair.martinez@evertecinc.com>
2021-05-10 22:05:33 -0500

    Documentation

--- config/payload.php
+++ config/payload.php
@@ -4,7 +4,7 @@ return [
     /**
      * Name parameter on receive payload. If send without name put null
      */
-    'param_name' => 'locations',
+    'param_name' => null,
 
     /**
      * On massive save, separate location by charater. Put null if not use massive save.
